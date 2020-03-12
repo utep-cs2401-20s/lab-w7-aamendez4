feafc86fcb2c6c5b69f3ae8f040ce4e0ca969c35
aamendez4 <aamendez4@utep.edu>
2020-03-11 23:32:05 -0600

    - implemented SortOfSort.java and SortOfSortTester.java

--- wk7lab/src/SortOfSort.java
+++ wk7lab/src/SortOfSort.java
@@ -1,15 +1,49 @@
 public class SortOfSort {
-    public static void sortOfSort(int[] arr) {
-        int max = Integer.MAX_VALUE;
-        int temp;
-        for (int i = 0; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                temp = arr[arr.length-1];
-                arr[arr.length-1] = max;
-                arr[i] = temp;
+    private int[] arr;
+    private int length;
+
+    public SortOfSort(int[] inputArr){
+        arr = inputArr;
+    }
+
+    public void sortOfSort(){
+        int max = 0;
+        int count = 0;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            if(count < 2) {
+                max = start;
+                for (int i = start; i <= end; i++){
+                    if(arr[i] >= arr[max]){
+                        max = i;
+                    }
+                }
+                int temp = arr[max];
+                arr[max] = arr[end];
+                arr[end] = temp;
+                count++;
+                end--;
+            }
+            else{
+                max = start;
+                for(int i = start; i <= end; i++){
+                    if(arr[i] >= arr[max]){
+                        max = i;
+                    }
+                }
+                int temp = arr[max];
+                arr[max] = arr[start];
+                arr[start] = temp;
+                start++;
+                count++;
+            }
+            if(count == 4){
+                count = 0;
             }
         }
     }
+
 }
 
--- wk7lab/src/SortOfSortTester.java
+++ wk7lab/src/SortOfSortTester.java
@@ -1,2 +1,75 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SortOfSortTester {
+
+    //This test case was chosen in order to first check if the method actually works like it is supposed to with small values.
+    @Test
+    public void test1(){
+        int[] A = {2, 9, 5, 8, 3, 4};
+
+        int[] B = {5, 4, 2, 3, 8, 9};
+
+        SortOfSort x = new SortOfSort(A);
+
+        x.sortOfSort();
+
+        assertArrayEquals(B, A);
+    }
+
+    //This test case was chosen in order to see how the method would act with values that are large and have different size digits.
+    @Test
+    public void test2(){
+        int[] A = {24, 2463, 652626, 2344, 54354, 23234};
+
+        int[] B = {23234, 2463, 24, 2344, 54354, 652626};
+
+        SortOfSort x = new SortOfSort(A);
+
+        x.sortOfSort();
+
+        assertArrayEquals(B, A);
+    }
+
+    //This test case was chosen in order to see how the method behaves with a mix of negative anf positive values.
+    @Test
+    public void test3(){
+        int[] A = {32141234, -32142134, 4325, 0, 25432, -1};
+
+        int[] B = {4325, 0, -32142134, -1, 25432, 32141234};
+
+        SortOfSort x = new SortOfSort(A);
+
+        x.sortOfSort();
+
+        assertArrayEquals(B, A);
+    }
+
+    //This test case was chosen in order to see how the method would handle and sort duplicated values in the array.
+    @Test
+    public void test4(){
+        int[] A = {42, 324, 42, 234, 35, 42};
+
+        int[] B = {42, 42, 35, 42, 234, 324};
+
+        SortOfSort x = new SortOfSort(A);
+
+        x.sortOfSort();
+
+        assertArrayEquals(B, A);
+    }
+
+    //This test case was chosen in order to see how the method would work with a larger array as well as an array with an odd amount of values in it.
+    @Test
+    public void test5(){
+        int[] A = {2, 9, 5, 8, 3, 4, 5, 4, 6};
+
+        int[] B = {6, 5, 4, 3, 2, 4, 5, 8, 9};
+
+        SortOfSort x = new SortOfSort(A);
+
+        x.sortOfSort();
+
+        assertArrayEquals(B, A);
+    }
 }
